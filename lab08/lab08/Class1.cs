f16561b454c2c8cb68d16aa8ebfcad3c2a9298da
Nikita <nikitakarebo810@gmail.com>
2022-11-04 02:47:24 +0300

    Created new delegate and events

---
+++ lab08/lab08/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace lab08
+{
+    class Programmer
+    {
+        public delegate void LANG(List<string> list);
+        public event LANG Delete;
+        public event LANG Mutate;
+
+        public void dele(List<string> list)
+        {
+            Console.Write("Ведите номер элемента, который хотите удалить(начиная с 0): ");
+            int num = int.Parse(Console.ReadLine());
+            list.RemoveAt(num);
+            Delete?.Invoke(list);
+        }
+
+        public void Perenos(List<string> list)
+        {
+            Random random = new Random();
+            List<string> NewList = list.OrderBy(item => random.Next()).ToList();
+            list.Clear();
+            list.AddRange(NewList);
+            Mutate?.Invoke(list);
+        }
+    }
+    class StringEditor
+    {
+        public static string RemovePunctuation(string str)
+        {
+            return Regex.Replace(str, "[.,;:]", string.Empty);
+        }
+
+        public static string AddSymbol(string str)
+        {
+            return str += "Lab08";
+        }
+
+        public static string ToUpper(string str)
+        {
+            return str.ToUpper();
+        }
+
+        public static string ToLower(string str)
+        {
+            return str.ToLower();
+        }
+
+        public static string RemoveSpace(string str)
+        {
+            return str.Replace(" ", string.Empty);
+        }
+    }
+}
--- lab08/lab08/Program.cs
+++ lab08/lab08/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer.LANG Language;
+            Programmer programmer = new Programmer();
+            List<string> LP = new List<string> { "Ruby", "C#", "Kotlin", "Pascal", "Python", "GoLang", "VisualBasic", "Dart", "JS", "Java" };
+
+            Console.Write("Список: ");
+            foreach (string item in LP)
+            {
+                Console.Write(item + "   ");
+            }
+            Console.WriteLine();
+
+            programmer.Delete += list =>
+            {
+                Console.Write("Измененный: ");
+                foreach (string item in LP)
+                {
+                    Console.Write(item + "   ");
+                }
+                Console.WriteLine();
+            };
+
+            programmer.Mutate += list =>
+            {
+                Console.Write("Перестановка: ");
+                foreach (string item in LP)
+                {
+                    Console.Write(item + "   ");
+                }
+                Console.WriteLine();
+            };
+
+            Language = programmer.dele;
+            Language += programmer.Perenos;
+            Language += programmer.Perenos;
+            Language += programmer.Perenos;
+
+            Language(LP);
+
+            Func<string, string> A;
+            string str = "Nikita; Karebo. made, this; str";
+            Console.WriteLine($"\n\nСтрока: {str}");
+            A = StringEditor.RemovePunctuation;
+            Console.WriteLine($"{A.Method.Name}: {A(str)}");
+            A = StringEditor.AddSymbol;
+            Console.WriteLine($"{A.Method.Name}: {A(str)}");
+            A = StringEditor.ToUpper;
+            Console.WriteLine($"{A.Method.Name}: {A(str)}");
+            A = StringEditor.ToLower;
+            Console.WriteLine($"{A.Method.Name}: {A(str)}");
+            A = StringEditor.RemoveSpace;
+            Console.WriteLine($"{A.Method.Name}: {A(str)}");
+
+            Console.ReadKey();
+        }
+    }
+}
