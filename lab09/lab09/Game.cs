73a3b4dd62864c9ee655045a833fd1bc4ec1f127
Nikita <nikitakarebo810@gmail.com>
2022-11-13 22:32:18 +0300

    added methods for each class and code for test program

--- lab09/lab09/Game.cs
+++ lab09/lab09/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,7 @@ namespace lab09
         public string Type
         {
             get { return type; }
-            set { }
+            set { type = value; }
         }
 
         public Game(string type)
