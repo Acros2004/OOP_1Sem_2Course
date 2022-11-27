1c2ae613d645ed949057af7f5df9bfafe7901f46
Nikita <nikitakarebo810@gmail.com>
2022-11-28 01:12:45 +0300

    created new classes KNSLog, KNSDiskInfo and added new methods

---
+++ lab12/lab12/KNSDiskInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12
+{
+    internal static class KNSDiskInfo
+    {
+        public static event Action<string> OnUpdate;
+
+        internal static void ShowFreeSpace(string diskName)
+        {
+            DriveInfo driveInfo = DriveInfo.GetDrives().Single(drive => drive.Name == diskName);
+            OnUpdate($"Free space on the disk {driveInfo.Name} = {driveInfo.AvailableFreeSpace} bytes");
+        }
+
+        internal static void ShowFileSystemInformation(string diskName)
+        {
+            DriveInfo driveInfo = DriveInfo.GetDrives().Single(drive => drive.Name == diskName);
+
+            OnUpdate($"File System Information {driveInfo.Name} :\n" +
+                     $"Type: {driveInfo.DriveType}\n" +
+                     $"Format: {driveInfo.DriveFormat}\n" +
+                     $"TotalSize: {driveInfo.TotalSize} bytes\n");
+        }
+    }
+}
---
+++ lab12/lab12/KNSLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12
+{
+    internal class KNSLog
+    {
+        int counterUpdate = 0;
+        private readonly string patch;
+
+        public KNSLog(string patch)
+        {
+            this.patch = patch;
+        }
+
+        public void WriteActionInFile(string messageUpdate)
+        {
+            using (StreamWriter sw = new StreamWriter(patch, true))
+            {
+                sw.WriteLine($"<><><><><><><><><><><><><> Action: {counterUpdate + 1} <><><><><><><><><><><><><>\n");
+                sw.WriteLine($"Time Update: {DateTime.Now.ToString()}\n");
+                sw.WriteLine(messageUpdate);
+                sw.WriteLine($"\n<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><");
+            }
+            counterUpdate++;
+        }
+    }
+}
