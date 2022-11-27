03efc28a6c10ea303c2f7fac1f570a66c69a1151
Nikita <nikitakarebo810@gmail.com>
2022-11-27 22:39:51 +0300

    added new static methods for class Reflector and created new class Product

--- lab11/lab11/Program.cs
+++ lab11/lab11/Program.cs
@@ -5,3 +5,49 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Reflection;
+
+namespace lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "lab11.Product";
+            using (StreamWriter sw = new StreamWriter(@"Reflection.txt", false, Encoding.Default))
+            {
+                sw.WriteLine("-----определение имени сборки------");
+                sw.WriteLine(Reflector.GetAssemblyName());
+                sw.WriteLine("-----публичные конструкторы----- ");
+                sw.WriteLine(Reflector.HasPublicConstructors(name));
+                sw.WriteLine("-----все общедоступные публичные методы------");
+                Reflector.PublicMethods(name, sw);
+                sw.WriteLine("-----информация о полях и свойствах класса------");
+                Reflector.FieldsAndProperties(name, sw);
+                sw.WriteLine("-----все реализованные классом интерфейсы------");
+                Reflector.Interfaces(name, sw);
+                sw.WriteLine("-----выводит по имени класса имена методов------");
+                Reflector.MethodType(name, "newPrice", sw);
+                sw.WriteLine("-----метод Invoke------");
+                Reflector.Invoke(name, "Print");
+                sw.WriteLine(Reflector.Create(name).ToString());
+                sw.WriteLine("****************************************************************************");
+            }
+            name = "System.Type";
+            using (StreamWriter sw = new StreamWriter(@"Reflection.txt", true, Encoding.Default))
+            {
+                sw.WriteLine("-----определение имени сборки------");
+                sw.WriteLine(Reflector.GetAssemblyName());
+                sw.WriteLine("-----публичные конструкторы----- ");
+                sw.WriteLine(Reflector.HasPublicConstructors(name));
+                sw.WriteLine("-----все общедоступные публичные методы------");
+                Reflector.PublicMethods(name, sw);
+                sw.WriteLine("-----информация о полях и свойствах класса------");
+                Reflector.FieldsAndProperties(name, sw);
+                sw.WriteLine("-----все реализованные классом интерфейсы------");
+                Reflector.Interfaces(name, sw);
+                sw.WriteLine("-----выводит по имени класса имена методов------");
+                Reflector.MethodType(name, "value", sw);
+            }
+        }
+    }
+}
---
+++ lab11/lab11/Reflector.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace lab11
+{
+    static class Reflector
+    {
+        public static string GetAssemblyName()
+        {
+            return typeof(Program).Assembly.GetName().FullName;
+        }
+
+        public static bool HasPublicConstructors(string name)
+        {
+            Type type = Type.GetType(name, false,true);
+            foreach (var item in type.GetConstructors())
+            {
+                if (item.IsPublic)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static IEnumerable<string> PublicMethods(string name, StreamWriter sw)
+        {
+            List<string> list = new List<string>();
+            Type type = Type.GetType(name, false, true);
+            foreach (MethodInfo method in type.GetMethods())
+            {
+                if (method.IsPublic)
+                {
+                    list.Add(method.Name);
+                    sw.WriteLine(method.Name);
+                }
+            }
+            return list;
+        }
+        public static IEnumerable<string> FieldsAndProperties(string name, StreamWriter sw)
+        {
+            List<string> list = new List<string>();
+            Type type = Type.GetType(name, false, true);
+            foreach (var item in type.GetFields())
+            {
+                list.Add(item.Name);
+                sw.WriteLine(item.Name);
+            }
+            foreach (var item in type.GetProperties())
+            {
+                list.Add(item.Name);
+                sw.WriteLine(item.Name);
+            }
+            return list;
+        }
+        public static IEnumerable<string> Interfaces(string name, StreamWriter sw)
+        {
+            List<string> list = new List<string>();
+            Type type = Type.GetType(name, false, true);
+            foreach (var item in type.GetInterfaces())
+            {
+                list.Add(item.Name);
+                sw.WriteLine(item.Name);
+            }
+            return list;
+        }
+        public static void MethodType(string name, string param, StreamWriter sw)
+        {
+            Type type = Type.GetType(name, false, true);
+            foreach (var item in type.GetMethods())
+            {
+                if (item.GetParameters().Any(e => e.Name == param))
+                {
+                    sw.WriteLine(item);
+                }
+            }
+        }
+        public static void Invoke(string name, string methodName)
+        {
+            try
+            {
+                Type type = Type.GetType(name, false, true);
+                object obj = Activator.CreateInstance(type);
+                string[] param = File.ReadAllLines(@"Invoke.txt");
+                MethodInfo method = type.GetMethod(methodName);
+                method.Invoke(obj, param);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static object Create(string name)
+        {
+            return Activator.CreateInstance(Type.GetType(name));
+        }
+    }
+}
