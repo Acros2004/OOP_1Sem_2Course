ea1a72e9fb5cc38f89bc3857ea0f5bca069d74a8
Nikita <nikitakarebo810@gmail.com>
2022-12-26 04:35:27 +0300

    added the loss of Singleton and Prototype

--- Lab_19-20/Lab19-20/Program.cs
+++ Lab_19-20/Lab19-20/Program.cs
@@ -6,11 +6,8 @@ namespace Lab19_20
     {
         static void Main(string[] args)
         {
-            /*1) Добавьте к лабораторной №17-18 два структурных паттерна. */
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Client client = new Client("Ivanov Ivan Ivanovich", new DateTime(1998, 9, 23), "JH67423");
-            Flight flight = new Flight(6, "Варшава", "Токио", new DateTime(2023, 2, 3, 13, 37, 00), "Boeing 737", 160, 393.21);
+            Client client = new Client("Nikita", new DateTime(1998, 9, 23), "JH67423");
+            Flight flight = new Flight(6, "Минск", "Москва", new DateTime(2023, 2, 3, 13, 37, 00), "Boeing 737", 160, 393.21);
 
             Console.WriteLine("---- Adapter ----");
             client.Travel(flight);
@@ -20,26 +17,21 @@ namespace Lab19_20
             ITravel busToAirport = new Adapter(bus);
             client.Travel(busToAirport);
 
-            /////////////////////////////////////////////////////////
-            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("\n---- Decorator ----");
-            flight = new FlightWithAirport("Lotnisko Chopina w Warszawie", "Narita International Airport", flight);
+            flight = new FlightWithAirport("Minsk", "Moskow Airport", flight);
             Console.WriteLine(flight.ToString());
 
-            //2) Добавьте три паттерна поведения.
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            //три паттерна поведения.
             Console.WriteLine("\n---- State ----");
             client.MakeOrder(flight);
 
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("\n---- Command ----");
+           // Console.WriteLine("\n---- Command ----");
             Flight flight1 = new Flight(4, "Барселона", "Берлин", new DateTime(2023, 1, 15, 1, 01, 00), "Boeing 737", 214, 230);
             Admin admin = new Admin();
             admin.SetCommand(new FlightOnCommand(flight1));
             admin.AddNewFlight();
             admin.CancelFlight();
 
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("\n---- Strategy ----");
             Bus bus1 = new Bus("Mercedes", 1243, new PetrolMove());
             bus1.Move();
